96a3f05ae7f0003cdf2468fb040609da9de53edf
Michael Hueter <mthueter@gmail.com>
2017-11-29 15:56:50 -0800

    App is better as a pure functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ToDoList from './ToDoList';
 
-class App extends Component {
-  render() {
-    return <ToDoList />;
-  }
-}
+const App = (props) => <ToDoList />;
 
 export default App;
