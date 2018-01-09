3128a397a9122da649d0ba9a0b8f978356c040d5
Michael Hueter <mthueter@gmail.com>
2018-01-09 14:12:24 -0800

    actions and reducers are complete

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,26 @@
 // constants aka Action Types
 export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
 
 // actionCreators
 function addTodo(newTodo) {
   return {
     type: ADD_TODO,
     newTodo
-  }
+  };
+}
+
+function removeTodo(id) {
+  return {
+    type: REMOVE_TODO,
+    id
+  };
+}
+
+function toggleTodo(id) {
+  return {
+    type: TOGGLE_TODO,
+    id
+  };
 }
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,4 +1,4 @@
-import { ADD_TODO } from './actions';
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from './actions';
 
 const DEFAULT_STATE = {
   todos: []
@@ -8,6 +8,19 @@ export const todos = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case ADD_TODO: {
       const newTodos = [...state.todos, action.newTodo];
+      return { ...state, todos: newTodos };
+    }
+    case REMOVE_TODO: {
+      const newTodos = state.todos.filter(todo => todo.id !== action.id);
+      return { ...state, todos: newTodos };
+    }
+    case TOGGLE_TODO: {
+      const newTodos = state.todos.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, applied: !todo.applied };
+        }
+        return todo;
+      });
       return { ...state, todos: newTodos };
     }
     default:
